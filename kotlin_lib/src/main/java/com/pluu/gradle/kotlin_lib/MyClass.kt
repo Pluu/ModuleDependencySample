70c1f6e8b35164fe5cfc37d16b7af80b58487663
pluu <pluulove@gmail.com>
2023-03-19 22:40:03 +0900

    Init module

---
+++ kotlin_lib/src/main/java/com/pluu/gradle/kotlin_lib/MyClass.kt
@@ -0,0 +1,4 @@
+package com.pluu.gradle.kotlin_lib
+
+class MyClass {
+}
---
+++ kotlin_lib/src/test/java/com/pluu/gradle/kotlin_lib/MyClassTest.kt
@@ -0,0 +1,11 @@
+package com.pluu.gradle.kotlin_lib
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MyClassTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
